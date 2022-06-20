d0ac575ea7c8554ef7f5c97582e40bce367d6af8
DionSat <43707506+DionSat@users.noreply.github.com>
2022-06-20 03:54:21 -0700

    added trivia project to my projects section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import water_image from "../Images/water.png";
 import deck_image from "../Images/blackjack.png";
 import git_logo from "../Images/github.png";
+import trivia_image from "../Images/trivia.png";
 import { Container, Row, Col } from "react-bootstrap";
 
 export default function Projects({ id }) {
@@ -90,6 +91,46 @@ export default function Projects({ id }) {
                 <a
                   className='btn git_button d-flex align-items-center justify-content-center'
                   href='https://github.com/DionSat/Deck-Of-Cards'
+                  target='_blank'
+                  role='button'>
+                  <img
+                    className='button_image'
+                    src={git_logo}
+                    alt='GitHub'
+                    width='90'
+                    height='90'
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xl={6} lg={12}>
+          <div className='project_container container'>
+            <div className='my_projects p-4'>
+              <img
+                src={trivia_image}
+                className='trivia-image'
+                alt='trivia game application'
+                width='100%'
+                height='100%'></img>
+              <h2 className='pt-4'>Trivia Game Application</h2>
+              <p className='text-primary deck_desc pt-4'>
+                Worked in a 3 man team to design a trivia multiplayer client 
+                and server for my full stack class. We used React for the
+                front end, node.js and socket.io for the backend and multiplayer,
+                docker and python for an API microserivice, and Postgres for
+                data storage like gamestate, users and leaderboard.
+              </p>
+              <p className='deck_info pt-4'>
+                A trivia multiplayer application that allows multiple users to
+                join, leave and play in the same room. This also features user login,
+                and a leaderboard for users. 
+              </p>
+              <div className='w-100 d-flex align-items-center justify-content-center'>
+                <a
+                  className='btn git_button d-flex align-items-center justify-content-center'
+                  href='https://github.com/DionSat/trivia_application'
                   target='_blank'
                   role='button'>
                   <img
