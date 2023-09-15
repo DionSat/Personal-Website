d0e94832e71d67747c774a98d74f3dc70f4082bc
DionSat <43707506+DionSat@users.noreply.github.com>
2023-09-15 13:04:19 -0700

    Made the terminal image more responsive

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -6,6 +6,7 @@ export default function ContactMe({ id }) {
   return (
     <Container fluid className='h-100 d-flex align-items-center' id={id}>
       <div className='w-100 h-100 row d-flex align-items-center'>
+        
       </div>
     </Container>
   );
